64edd6cba716c50ea1093060e456dcff4d9bde75
Favvie <favourprecious3@gmail.com>
2022-10-24 15:54:54 +0100

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 import os
 
 app = Flask(__name__)
@@ -19,9 +19,9 @@ def close_session(exc):
 @app.errorhandler(404)
 def page_not_found(error):
     """custom error handler"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=(os.getenv("HBNB_API_HOST") or "0.0.0.0"),
-            port=(os.getenv("HBNB_API_PORT") or 5000), threaded=True)
+    app.run(host=(os.getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=(os.getenv("HBNB_API_PORT", "5000"), threaded=True)
