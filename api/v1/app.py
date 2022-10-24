3365e163c25034ca3a0832414b356606c51de04f
Favvie <favourprecious3@gmail.com>
2022-10-24 21:44:29 +0100

    add custom 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """root app module"""
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def close_storage(exc):
     """a function that closes db connection after each request"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """return a custom json response for 404 pages"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
