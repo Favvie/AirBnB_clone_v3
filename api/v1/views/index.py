005e88d09849471166978088f6f62223d2a8a028
Favvie <favourprecious3@gmail.com>
2022-10-24 21:30:49 +0100

    add stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenity": Amenity, "city": City, "place": Place,
+           "review": Review, "state": State, "user": User}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """method to return status of route"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """count the number of instances for each class"""
+    classes_count = {}
+    for key, value in classes.items():
+        classes_count[key] = storage.count(value)
+    return jsonify(classes_count)
